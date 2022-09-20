482f04027c56d84aef12d2a71640c06fc3979d01
christopherpettinga <chrispettinga@gmail.com>
2022-09-20 12:02:29 +0100

    Removed unused function

--- v2_api_client/library/organisations.py
+++ v2_api_client/library/organisations.py
@@ -4,9 +4,6 @@ from v2_api_client.library import BaseAPIClient
 class OrganisationAPIClient(BaseAPIClient):
     base_endpoint = "organisations"
 
-    def add_user_to_organisation(self):
-        pass
-
 
 class OrganisationCaseRoleAPIClient(BaseAPIClient):
     base_endpoint = "organisation_case_roles"
