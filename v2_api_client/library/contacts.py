7c6cb58cd49463eabec16f7bebd101eb9f059c4f
christopherpettinga <chrispettinga@gmail.com>
2022-10-24 12:39:57 +0100

    added change_org method to Contact object

--- v2_api_client/library/contacts.py
+++ v2_api_client/library/contacts.py
@@ -1,5 +1,18 @@
 from v2_api_client.library import BaseAPIClient
+from v2_api_client.trs_object import TRSObject
+
+
+class ContactObject(TRSObject):
+    def change_organisation(self, organisation_id):
+        return self.custom_action(
+            "patch",
+            "change_organisation",
+            data={
+                "organisation_id": organisation_id,
+            }
+        )
 
 
 class ContactsAPIClient(BaseAPIClient):
     base_endpoint = "contacts"
+    trs_object_class = ContactObject
