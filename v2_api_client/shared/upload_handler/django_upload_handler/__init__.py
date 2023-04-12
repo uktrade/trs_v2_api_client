22dd560c8ceeddb1b72725c654f800a38a74fd8e
christopherpettinga <chrispettinga@gmail.com>
2023-04-12 10:39:14 +0100

    Error now computed in client level

--- v2_api_client/shared/upload_handler/django_upload_handler/__init__.py
+++ v2_api_client/shared/upload_handler/django_upload_handler/__init__.py
@@ -3,6 +3,10 @@ from django.core.files.uploadhandler import FileUploadHandler, StopUpload
 
 from v2_api_client.shared.upload_handler.metadata import Extractor
 
+FILE_MAX_SIZE_BYTES_ERROR = (
+    f"The selected file must be smaller than "
+    f"{round(settings.FILE_MAX_SIZE_BYTES / (1024 * 1024))}MB"
+)
 
 class ExtractMetadataFileUploadHandler(FileUploadHandler):
     def file_complete(self, file_size):
@@ -10,7 +14,7 @@ class ExtractMetadataFileUploadHandler(FileUploadHandler):
 
     def receive_data_chunk(self, raw_data, start):
         if len(raw_data) > settings.FILE_MAX_SIZE_BYTES:
-            raise StopUpload(settings.FILE_MAX_SIZE_BYTES_ERROR)
+            raise StopUpload(FILE_MAX_SIZE_BYTES_ERROR)
 
         extractor = Extractor()
         sanitised_data = extractor(raw_data, self.content_type)
