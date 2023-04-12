c851c1c799aa2e2414bff1b51f4d7ce3f0083cc2
christopherpettinga <chrispettinga@gmail.com>
2023-04-12 10:16:53 +0100

    Rejigging around the django upload handler stuff
    TRSV2-535

---
+++ v2_api_client/shared/upload_handler/django_upload_handler/__init__.py
@@ -0,0 +1,17 @@
+from django.conf import settings
+from django.core.files.uploadhandler import FileUploadHandler, StopUpload
+
+from v2_api_client.shared.upload_handler.metadata import Extractor
+
+class ExtractMetadataFileUploadHandler(FileUploadHandler):
+    def file_complete(self, file_size):
+        pass
+
+    def receive_data_chunk(self, raw_data, start):
+        if len(raw_data) > settings.FILE_MAX_SIZE_BYTES:
+            raise StopUpload(settings.FILE_MAX_SIZE_BYTES_ERROR)
+
+        extractor = Extractor()
+        sanitised_data = extractor(raw_data, self.content_type)
+
+        return sanitised_data.getvalue()
