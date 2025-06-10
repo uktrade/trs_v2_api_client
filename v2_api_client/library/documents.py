0b50730ba25a7dda3b2e075f4637325e26cd50a3
abarolo <asmundbekker@DBT000435.local>
2025-06-10 10:49:08 +0100

    init

--- v2_api_client/library/documents.py
+++ v2_api_client/library/documents.py
@@ -4,6 +4,12 @@ from v2_api_client.library import BaseAPIClient
 class DocumentsAPIClient(BaseAPIClient):
     base_endpoint = "documents"
 
+    def get_presigned_url(self, filename):
+        return self.post(
+            self.url("documents"),
+            data={"filename": filename},
+        )
+
 
 class DocumentBundlesAPIClient(BaseAPIClient):
     base_endpoint = "document_bundles"
