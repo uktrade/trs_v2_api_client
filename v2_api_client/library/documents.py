9e5cd08d0c2271c636d70e11f9a011f83d5adc09
abarolo <asmundbekker@DBT000435.local>
2025-06-12 15:02:22 +0100

    add av results

--- v2_api_client/library/documents.py
+++ v2_api_client/library/documents.py
@@ -4,10 +4,16 @@ from v2_api_client.library import BaseAPIClient
 class DocumentsAPIClient(BaseAPIClient):
     base_endpoint = "documents"
 
-    def get_presigned_url(self, filename):
+    def get_presigned_url(self, filename, av_scanned_at=None, av_passed=None):
+        data = {"filename": filename}
+        if av_passed:
+            data["av_passed"] = av_passed
+        if av_scanned_at:
+            data["av_scanned_at"] = av_scanned_at
+
         return self.post(
             self.url("documents/s3-presigned-url"),
-            data={"filename": filename},
+            data=data,
         )
 
 
