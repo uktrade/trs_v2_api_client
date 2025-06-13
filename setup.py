152124b9e4e745630bf8d91a07902ed42a64cb3d
Uka Osim <uka.osim@digital.trade.gov.uk>
2025-06-13 13:47:35 +0100

    Create version.py (#36)
    * Create version.py
    
    * added version
    
    * importing version

--- setup.py
+++ setup.py
@@ -1,11 +1,13 @@
 import os
 from setuptools import find_packages, setup
+from v2_api_client.version import __version__
 
 # allow setup.py to be run from any path
 os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name="v2-api-client",
+    version=__version__,
     packages=find_packages(),
     include_package_data=True,
     license="MIT",
